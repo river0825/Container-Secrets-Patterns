f5d20ea6d9ee67648780b7842eba4047dfead1f5
river0825 <riverdm@gmail.com>
2020-03-01 21:52:09 +0800

    Add k8s script

--- init-k8s-secret.sh
+++ init-k8s-secret.sh
@@ -30,5 +30,12 @@ echoAndWait "echo -n 'password-of-ftp' > ./ftp-password"
 echoAndWait "echo -n 'dbadm' > ./db-username"
 echoAndWait "echo -n 'password-of-db' > ./db-password"
 
-echoAndWait "kubectl create secret generic ftp-user-pass --from-file=./ftp-username --from-file=./ftp-password" "建立 ftp password"
-echoAndWait "kubectl create secret generic db-user-pass --from-file=./db-username --from-file=./db-password" "建立 db password"
+echoAndWait "kubectl create secret generic ftp-user-pass --from-file=./ftp-username --from-file=./ftp-password" "${MARK}#建立 ftp password${NC}"
+echoAndWait "kubectl create secret generic db-user-pass --from-file=./db-username --from-file=./db-password" "${MARK}建立 db password${NC}"
+echoAndWait "kubectl get secrets" "${MARK}取得 secret${NC}"
+echoAndWait "kubectl describe secrets/db-user-pass" "${MARK}取得 secret 的詳細資料${NC}"
+
+echo Step "2. 存取控管 - 將 secret 佈署至 pod 中"
+echoAndWait "kubectl apply -f https://raw.githubusercontent.com/river0825/Container-Secrets-Patterns/master/secret-envars-pod.yml" "${MARK}啟動 pod${NC}"
+echoAndWait "kubectl exec -it secret-envars-test-pod -- /bin/bash" "${MARK}進入 POD 中，接下來需要手動執行${NC}"
+
