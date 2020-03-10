ba82e58eca8be381491e371c9e8ea0adfc5b2f02
river0825 <river0825@gamil.com>
2020-03-10 17:27:10 +0800

    Add a step

--- init-k8s-secret.sh
+++ init-k8s-secret.sh
@@ -30,12 +30,13 @@ echoAndWait "echo -n 'password-of-ftp' > ./ftp-password"
 echoAndWait "echo -n 'dbadm' > ./db-username"
 echoAndWait "echo -n 'password-of-db' > ./db-password"
 
+echoStep "2. 將 secret 建立到 k8s 的中央環境中"
 echoAndWait "kubectl create secret generic ftp-user-pass --from-file=./ftp-username --from-file=./ftp-password" "${MARK}# 建立 ftp password${NC}"
 echoAndWait "kubectl create secret generic db-user-pass --from-file=./db-username --from-file=./db-password" "${MARK}# 建立 db password${NC}"
 echoAndWait "kubectl get secrets" "${MARK}# 取得 secret${NC}"
 echoAndWait "kubectl describe secrets/db-user-pass" "${MARK}# 取得 secret 的詳細資料${NC}"
 
-echo Step "2. 存取控管 - 將 secret 佈署至 pod 中"
+echo Step "3. 存取控管 - 將 secret 佈署至 pod 中"
 echoAndWait "kubectl apply -f https://raw.githubusercontent.com/river0825/Container-Secrets-Patterns/master/secret-envars-pod.yml" "${MARK}# 啟動 pod${NC}"
 echoAndWait "kubectl exec -it secret-envars-test-pod -- /bin/bash -c 'printenv | grep -i SECRET'" "${MARK}# 進入 POD 中，將 secret 列出來，可以看到剛剛設定的 secret 已經變成環境變數了${NC}"
 
